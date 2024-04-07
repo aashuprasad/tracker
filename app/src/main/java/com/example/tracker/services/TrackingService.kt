2900fd09028485108f5db430f045f53464d9f8f7
aashuprasad <aashuprasad603@gmail.com>
2024-04-07 18:34:45 +0530

    drawing running track

--- app/src/main/java/com/example/tracker/services/TrackingService.kt
+++ app/src/main/java/com/example/tracker/services/TrackingService.kt
@@ -123,10 +123,13 @@ class TrackingService : LifecycleService(){
                         isFirstRun = false
                     } else {
                         Timber.d("Resuming service...")
+                        startForegroundService()
                     }
                 }
                 ACTION_PAUSE_SERVICE->{
                     Timber.d("Paused service")
+                    pauseService()
+
                 }
                 ACTION_STOP_SERVICE->{
                     Timber.d("Stopped service")
@@ -134,6 +137,10 @@ class TrackingService : LifecycleService(){
             }
         }
         return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun pauseService() {
+        isTracking.postValue(false)
     }
 
 
